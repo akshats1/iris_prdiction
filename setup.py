584087b7dafe5bec63e3cbc817a37b9a5827779b
akshats1 <akshats063@gmail.com>
2024-04-25 23:47:32 +0530

    Files Uploaded

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="iris",
+    version="0.0.1",
+    author="Akshat",
+    author_email="akshats063@gmail.com",
+    packages=find_packages()
+)
